56bf465995572e840cb719f7be7eb317ec16b374
oleksii vovnenko <kespa71for@gmail.com>
2022-11-15 00:05:09 +0300

    create component Header.tsx

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from 'react';
+import style from '../Header/Header.module.css';
+
+export const Header:FC  = () =>  {
+  return (
+    <header className={style.header}>
+      <div className={style.headerContainer}>
+        <header className={style.headerWrapper}>
+          <div className={style.headerLeft}>Logo</div>
+          <div className={style.headerRight}>Navigation</div>
+        </header>
+      </div>
+    </header>
+  )
+} 
