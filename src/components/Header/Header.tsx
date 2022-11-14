e2f442e3777729ec66cb0113d7fd6f5de6d9f7e5
oleksii vovnenko <kespa71for@gmail.com>
2022-11-15 00:10:37 +0300

    create componet Logo.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,15 @@
 import React, { FC } from 'react';
+import { Logo } from '../Logo/Logo';
 import style from '../Header/Header.module.css';
 
-export const Header:FC  = () =>  {
+export const Header:FC = () =>  {
   return (
     <header className={style.header}>
       <div className={style.headerContainer}>
         <header className={style.headerWrapper}>
-          <div className={style.headerLeft}>Logo</div>
+          <div className={style.headerLeft}>
+            <Logo/>
+          </div>
           <div className={style.headerRight}>Navigation</div>
         </header>
       </div>
---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react';
+import style from '../Logo/Logo.module.css';
+
+
+export const Logo :FC = () => {
+  return (
+    <div className={style.logo}>Logo</div>
+  )
+}
